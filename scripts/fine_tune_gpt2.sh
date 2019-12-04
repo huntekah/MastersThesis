2ce66e0b6d189f0289a060e69bb20331ef39ab88
Jurkiewicz Krzysztof <huntekah-remote>
2019-12-04 14:36:33 +0100

    compute results for gpt2-oddballness with bos_token added

--- scripts/fine_tune_gpt2.sh
+++ scripts/fine_tune_gpt2.sh
@@ -1,8 +1,8 @@
-dir="../oddballness-paper/gonito_challenge_data/challenge/dev-100/"
+dir="../oddballness-paper/gonito_challenge_data/challenge/dev-0/"
 LC_NUMERIC=C
 echo -e "threshold\tMean/Multilabel-F2.0\tMean/Multilabel-F2.0\talpha" | tee -a results_gpt2.tmp
-for alpha in $(seq 0.05 0.05 5.0); do
+for alpha in $(seq 0.05 0.10 7.0); do
   alpha=$(echo $alpha | tr "," ".")
   result=$(python gonito_infer_gpt2.py --file ${dir}in.tsv --out ${dir}out.tsv --alpha $alpha --expected ${dir}expected.tsv)
-  echo -e "${result}" | tee -a results_gpt2.tmp
+  echo -e "${result}" | tee -a results_gpt2_bos.tmp
 done
