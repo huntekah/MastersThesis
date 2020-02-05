2a7dd34bc02492316c02cc1459d36e47500212a8
Jurkiewicz Krzysztof <huntekah-remote>
2020-02-06 00:37:32 +0100

    baseline results

--- scripts/eval_baseline.sh
+++ scripts/eval_baseline.sh
@@ -1,13 +1,14 @@
 LC_NUMERIC=C
 alpha=1.0
-threshold=0.909
-challenge_dir="../oddballness-paper/gonito_challenge_data/challenge"
-results_dir="${challenge_dir}/gpt2-oddballness-bos-a${alpha}-t${threshold}"
+threshold=0.09375
+#challenge_dir="../oddballness-paper/gonito_challenge_data/challenge"
+challenge_dir="../oddballness-paper/challenge_v6.2/";
+results_dir="${challenge_dir}/gpt2-probability-a${alpha}-t${threshold}"
 mkdir -p $results_dir
 
 for folder in dev-0 test-A; do
 	dir="${challenge_dir}/${folder}/"
 	mkdir -p $results_dir/$folder
-	python gonito_infer_gpt2_probability_baseline.py --file ${dir}in.tsv --out ${results_dir}/${folder}/out.tsv --alpha $alpha --threshold $threshold
+	python gonito_infer_gpt2_probability_baseline.py --file ${dir}in.tsv --out ${results_dir}/${folder}/out.tsv --alpha $alpha --threshold $threshold --detokenized ${dir}in_detokenized.tsv 
 	echo Result for "${folder}" saved to "${results_dir}"
 done
