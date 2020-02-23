cfbc2389f823933a59dae5eb7cb51f99e4cd3ebf
huntekah <huntekah@gmail.com>
2020-02-23 13:41:12 +0100

    add config for exhaustive job

---
+++ scripts/CONFIG.exhaustive.sh
@@ -0,0 +1,6 @@
+dir="../oddballness-paper/challenge_v6.2/dev-0/";
+complexity=20
+file_name=results_gpt2_exhaustive_${complexity}.tmp
+START=0.75
+STOP=2.0
+STEP=0.5
--- scripts/fine_tune_gpt2_exhaustive.sh
+++ scripts/fine_tune_gpt2_exhaustive.sh
@@ -3,8 +3,9 @@ dir="../oddballness-paper/challenge_v6.2/dev-0/";
 LC_NUMERIC=C
 complexity=20
 file_name=results_gpt2_exhaustive_${complexity}.tmp
+. ./CONFIG.exhaustive.sh
 echo -e "threshold\tMean/Multilabel-F2.0\tMean/Multilabel-F2.0\talpha" | tee -a $file_name
-for alpha in $(seq 0.75 0.05 1.4); do
+for alpha in $(seq $START $STEP $STOP); do
   alpha=$(echo $alpha | tr "," ".")
   result=$(python gonito_infer_gpt2_exhaustive.py --file ${dir}in.tsv --out ${dir}out.tsv --alpha $alpha --detokenized ${dir}in_detokenized.tsv  --expected ${dir}expected.tsv --complexity ${complexity})
   echo -e "${result}" | tee -a $file_name
