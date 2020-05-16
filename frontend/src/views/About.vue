1e9026840a06ea9c36a3795c9a2df31547299635
huntekah <huntekah@gmail.com>
2020-05-16 16:09:41 +0200

    add card like style to about page

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,17 +1,30 @@
 <template>
-  <div class="about">
-    <div class="d-flex justify-content-center">
-      <img alt="Penguin logo" src="../assets/penguin.png" height="110px">
+<div class="about">
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <div class="d-flex justify-content-center">
+          <img alt="Penguin logo"
+               src="../assets/penguin.png"
+               height="110px">
+        </div>
+      </div>
     </div>
-    <p>
-      This is the frontend of my Masters Thesis project. <br>
-      You can correct your english.txt file or write a nice poem!
-    </p>
+    <div class="row justify-content-md-center">
+      <div class="col-lg-6 shadow">
+
+        <p>
+          This is the frontend of my Masters Thesis project. <br>
+          You can correct your english.txt file or write a nice poem!
+        </p>
+      </div>
   </div>
+</div>
+</div>
 </template>
 
 <style>
-.about{
+.about {
   text-align: center;
 }
 </style>
